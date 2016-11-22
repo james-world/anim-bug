38d323c53ebdc046bb7f523cc35f338582f71e5a
James World <james-world@users.noreply.github.com>
2016-11-22 18:03:08 +0000

    Add transition on transition completion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import {
   style,
   transition,
   animate,
+  AnimationTransitionEvent,
+  ChangeDetectorRef,
   OnInit } from '@angular/core';
 
 @Component({
@@ -17,13 +19,19 @@ import {
       state('faded', style({
         opacity: 0
       })),
+      state('done', style({
+        opacity: 0
+      })),            
       transition('* => faded', animate('1s ease-out'))
     ]),
     trigger('text2state', [
       state('faded', style({
-        opacity: 1
+        opacity: 0
       })),
-      transition('* => faded', animate('1s ease-in'))
+      state('done', style({
+        opacity: 1
+      })),      
+      transition('faded => done', animate('1s ease-in'))
     ])    
   ]
 })
@@ -31,7 +39,21 @@ export class AppComponent implements OnInit {
   title = 'app works!';
   state: string;
 
+  constructor(private cdr: ChangeDetectorRef) {
+
+  }
+
   ngOnInit() {
     this.state = 'faded';
+  }
+
+  text1animated(evt: AnimationTransitionEvent) {
+    console.log(evt);
+    if(evt.toState == 'faded')
+    {
+      this.state = 'done';
+      // this line is required for transition to fire
+      this.cdr.detectChanges();
+    }
   }
 }
