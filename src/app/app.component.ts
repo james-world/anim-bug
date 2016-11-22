e894a559107cb7d76c345cd5b543fe3dfce5c3e6
James World <james-world@users.noreply.github.com>
2016-11-22 17:43:27 +0000

    Simple parallel animation working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('text1state', [
+      state('faded', style({
+        opacity: 0
+      })),
+      transition('* => faded', animate('1s ease-out'))
+    ]),
+    trigger('text2state', [
+      state('faded', style({
+        opacity: 1
+      })),
+      transition('* => faded', animate('1s ease-in'))
+    ])    
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  state: string;
+
+  ngOnInit() {
+    this.state = 'faded';
+  }
 }
